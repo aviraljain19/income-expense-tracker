de97b9799c7a5207355af59e087410da930d7053
Aviral Jain <aviraljainltr@gmail.com>
2024-03-06 19:12:21 +0530

    MVC Pattern: Modelling
    Signed-off-by: Aviral Jain <aviraljainltr@gmail.com>

---
+++ Server/model/Account.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    accountType: {
+      type: String,
+      enum: [
+        "Savings",
+        "Investment",
+        "Checking",
+        "Credit Card",
+        "Building",
+        "School",
+        "Project",
+        "Utilities",
+        "Travel",
+        "Personal",
+        "Groceries",
+        "Entertainment",
+        "Loan",
+        "Cash Flow",
+        "Uncategorized",
+      ],
+      required: true,
+    },
+    initialBalance: {
+      type: Number,
+      default: 0,
+    },
+    transaction: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Transaction",
+      },
+    ],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    notes: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
+
+const Account = mongoose.model("Account", accountSchema);
+
+module.exports = Account;
