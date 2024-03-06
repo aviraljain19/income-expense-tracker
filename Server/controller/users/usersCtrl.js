fc95d458a1104bf874d7bd8d841774c01a718f39
Aviral Jain <aviraljainltr@gmail.com>
2024-03-06 22:22:26 +0530

    User registration
    Signed-off-by: Aviral Jain <aviraljainltr@gmail.com>

--- Server/controller/users/usersCtrl.js
+++ Server/controller/users/usersCtrl.js
@@ -1,6 +1,27 @@
+const bcrypt = require("bcryptjs");
+const User = require("../../model/User");
+
 const registerUserCtrl = async (req, res) => {
+  const { fullname, password, email } = req.body;
   try {
-    res.json({ msg: "Register Route" });
+    const userFound = await User.findOne({ email });
+    if (userFound) return res.json({ message: "User already exists" });
+    if (!email || !password || !fullname) {
+      return res.json({ message: "Please fill all the details" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await User.create({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+    res.json({
+      status: "Success",
+      fullname: user.fullname,
+      email: user.email,
+      id: user._id,
+    });
   } catch (error) {
     res.json(error);
   }
--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,8 @@ const transactionsRoute = require("./routes/transactions/transactionRoutes");
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/v1/user", usersRoute);
 
 app.use("/api/v1/account", accountsRoute);
