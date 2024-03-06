5b35a9e52125a65a5c0680e2669997278c71f70c
Aviral Jain <aviraljainltr@gmail.com>
2024-03-07 01:07:04 +0530

    Authorization: Verify token
    Signed-off-by: Aviral Jain <aviraljainltr@gmail.com>

--- Server/controller/users/usersCtrl.js
+++ Server/controller/users/usersCtrl.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const User = require("../../model/User");
 const appErr = require("../../utils/AppErr");
 const generateToken = require("../../utils/generateToken");
+const verifyToken = require("../../utils/verigyToken");
 
 const registerUserCtrl = async (req, res, next) => {
   const { fullname, password, email } = req.body;
@@ -54,6 +55,10 @@ const loginUserCtrl = async (req, res, next) => {
 };
 
 const userProfileCtrl = async (req, res) => {
+  const result = verifyToken(
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZTg5YWZmMTc3YzA3Y2JhMDIyMWRlNSIsImlhdCI6MTcwOTc1MzMxOSwiZXhwIjoxNzA5ODM5NzE5fQ.WIO63li2UGWNz38xWLFtem3rSXrJmGipYebny5dOyEs"
+  );
+  console.log(result);
   try {
     res.json({ msg: "Profile Route" });
   } catch (error) {
