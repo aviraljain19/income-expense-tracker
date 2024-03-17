09874032721ae448b3c227c3297e762c56839a2e
Aviral Jain <aviraljainltr@gmail.com>
2024-03-17 23:14:42 +0530

    Account details component
    Signed-off-by: Aviral Jain <aviraljainltr@gmail.com>

--- Client/income-expense-tracker/src/App.js
+++ Client/income-expense-tracker/src/App.js
@@ -6,6 +6,7 @@ import Register from "./components/Forms/Register";
 import Navbar from "./components/Navbar/Navbar";
 import AddTransaction from "./components/Forms/AddTransaction";
 import AccountDashboard from "./components/Dashboard/AccountDashboard";
+import AccountDetails from "./components/Dashboard/AccountDetails";
 
 function App() {
   return (
@@ -17,6 +18,10 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/add-transaction" element={<AddTransaction />} />
         <Route path="/dashboard" element={<AccountDashboard />} />
+        <Route
+          path="/account-details/:accountID"
+          element={<AccountDetails />}
+        />
       </Routes>
     </BrowserRouter>
   );
